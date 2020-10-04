5e189787e28daba134f7e4f27ebc1c588f46f378
hanbyul9 <beammeupplzz@gmail.com>
2020-10-04 16:50:19 +0900

    feat : spring secure 반영중

--- ynm/src/main/java/kr/ko/ym/common/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package kr.ko.ym.common.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- ynm/src/main/java/kr/ko/ym/common/security/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package kr.ko.ym.common.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-}
